0d95124bd94deaa268435ea5689f041dc448e43d
hmazomba_deviare <hlumisa.mazomba@deviare.africa>
2022-09-13 01:18:34 +0200

    Nice Cube :)

--- src/stories/ChakraCard.jsx
+++ src/stories/ChakraCard.jsx
@@ -102,9 +102,9 @@ ProductCard.propTypes = {
 };
 
 ProductCard.defaultProps = {
-  image: "https://images.unsplash.com/photo-1518051870910-a46e30d9db16?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80",
-  productName: "Chair",
-  desc: 'Nice Chair',
-  salePrice: 'R150',
-  originalPrice: 'R450'
+  image: "https://images.unsplash.com/photo-1599394407175-b6da85464b90?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZnV0dXJpc3RpY3xlbnwwfHwwfHw%3D&w=1000&q=80",
+  productName: "a nice cube",
+  desc: 'Cube From Space',
+  salePrice: 'R150 000',
+  originalPrice: 'R450 000'
 };
